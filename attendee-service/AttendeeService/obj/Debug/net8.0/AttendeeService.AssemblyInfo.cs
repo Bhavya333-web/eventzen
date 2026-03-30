d00e1e6d2377e78f6a5c8a08dfa4afe20f1a9351
Bhavya Mittal <bhavmittal.ext@deloitte.com>
2026-03-30 18:48:00 +0530

    Added final project documentation (PDF & DOCX)

--- attendee-service/AttendeeService/obj/Debug/net8.0/AttendeeService.AssemblyInfo.cs
+++ attendee-service/AttendeeService/obj/Debug/net8.0/AttendeeService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttendeeService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+873bb5ba9dd1e12f65cd03b691499798033ea7bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082bf69fa0619fb7d8140fa82543657ffa28d41f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttendeeService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttendeeService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
